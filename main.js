ef7d1425baae6c369ad2d510905fc09ba79cb800
ezzkes <95560170+ezzkes@users.noreply.github.com>
2021-12-15 20:50:43 -1000

    Update main.js

--- main.js
+++ main.js
@@ -58,3 +58,15 @@ console.log("g"); }
 console.log("l"); }
   if(keyPressed == '84') { new_image('trunk.jpg');
 console.log("t"); } 
+  if(keyPressed == '89) { new_image('yellow_wall.png');
+console.log("y"); }
+  if(keyPressed == '82') { new_image('roof.jpg');
+console.log("r"); }
+  if(keyPressed == '68') { new_image('dark_green.png');
+console.log("d"); }
+  if(keyPressed == '85') { new_image('uniqe.png');
+console.log("u"); }
+  if(keyPressed == '67') { new_image('cloud.jpg');
+console.log("c"); }
+}
+funtion up()
